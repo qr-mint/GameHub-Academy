5e176bbb647d137a1d5fcb83aade5dc805a4592e
Victor Dmitrishen <victordmitrishen@Victors-Air.lan>
2026-09-12 01:14:46 +0500

    fix

--- game-front/src/api/game/leadboard.ts
+++ game-front/src/api/game/leadboard.ts
@@ -1,1 +1,5 @@
 import { apiClient } from '../request';
+
+export const getLeaderboardByTourname = () => {
+
+};
--- game-front/src/pages/leadboard.tsx
+++ game-front/src/pages/leadboard.tsx
@@ -13,7 +13,7 @@ export function LeaderboardPage ({ t }: LeaderboardPageProps) {
 			} catch {
 				
 			}
-		}
+		};
 		load();
 	}, []);
 
